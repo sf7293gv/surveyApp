bdcddcbcc75877010432a891afdbf864e689b5fb
Sonic <sf7293gv@go.minnstate.edu>
2022-03-02 12:48:59 -0600

    Added a new activity. Removed reset button

---
+++ app/src/main/java/com/example/surveyapp/SurveyResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.surveyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SurveyResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey_result)
+    }
+}
